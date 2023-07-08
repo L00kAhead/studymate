eecf8c49149706ca01ce4078679f797afb5c7c2b
l00kAhead <himanshusoni0925@gmail.com>
2023-07-08 15:35:35 +0700

    added models Topic & Message and modified Room model.

--- base/views.py
+++ base/views.py
@@ -3,12 +3,6 @@ from .models import Room
 
 # Create your views here.
 
-# rooms = [
-#     {"id": 1, "name": "UI/UX principles"},
-#     {"id": 2, "name": "Natural Language Processing"},
-# ]
-
-
 def home(request):
     rooms = Room.objects.all()
     return render(request, "base/home.html", {"rooms": rooms})
